67faf6db686183b6b0fd4e08ba68aa170a0d4ae7
Lakshay Madhav <lakshaymadhav25@gmail.com>
2021-12-24 13:41:40 +0530

    powerselect addon

--- addon/routes/post/edit.js
+++ addon/routes/post/edit.js
@@ -1,3 +1,11 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
-export default class PostEditRoute extends Route {}
+export default class PostEditRoute extends Route {
+  option = ['public', 'community', 'private'];
+  type = 'public';
+  @action
+  foo(selecttype) {
+    this.set('type', selecttype);
+  }
+}
