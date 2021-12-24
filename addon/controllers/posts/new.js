d669d649c4f5d8637474abd2a53a5bff3e1c8f73
Lakshay Madhav <lakshaymadhav25@gmail.com>
2021-12-24 09:35:51 +0530

    powerselect

--- addon/controllers/posts/new.js
+++ addon/controllers/posts/new.js
@@ -4,4 +4,9 @@ import { action } from '@ember/object';
 export default class PostsNewController extends Controller {
   option = ['public', 'community', 'private'];
   type = 'public';
+
+  @action
+  chooseType(choosed){
+    this.set('type', choosed);
+  }
 }
