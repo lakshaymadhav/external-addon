f42ad0082d3fb2a64e77ef2bbe58770d1df7ac29
Lakshay Madhav <lakshaymadhav25@gmail.com>
2021-12-24 09:34:30 +0530

    powerselect commit

--- addon/controllers/posts/new.js
+++ addon/controllers/posts/new.js
@@ -4,9 +4,4 @@ import { action } from '@ember/object';
 export default class PostsNewController extends Controller {
   option = ['public', 'community', 'private'];
   type = 'public';
-
-  @action
-  chooseType(mode) {
-    this.set('type', mode);
-  }
 }
